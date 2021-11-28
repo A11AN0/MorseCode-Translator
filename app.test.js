59824ba85854e853cb68db66748eaab95a961501
Allan <matrixboston7613@gmail.com>
2021-11-29 01:14:10 +0800

    successful tests for all singleCharacterConverter getters

--- app.test.js
+++ app.test.js
@@ -1,47 +1,81 @@
-import { singleLettersAndNumbersToMorse } from "./app.js";
+import { singleCharacterConverter } from "./app.js";
 
-describe("Tests for the singleLettersAndNumbersToMorse class", () => {
+describe("Tests for the singleCharacterConverter class", () => {
   describe("isLetterOrNumber Validator Test", () => {
     test("A single letter input (a-z or A-Z) should return true", () => {
-      const input = new singleLettersAndNumbersToMorse("a");
+      const input = new singleCharacterConverter("a");
       expect(input.isLetterOrNumber).toBe(true);
     });
 
     test("A single number input (0-9) should return true", () => {
-      const input = new singleLettersAndNumbersToMorse(7);
+      const input = new singleCharacterConverter(7);
       expect(input.isLetterOrNumber).toBe(true);
     });
 
     test("Anything that is not a letter, number, or (! , ? .) should return false", () => {
-      const input = new singleLettersAndNumbersToMorse("%");
+      const input = new singleCharacterConverter("%");
       expect(input.isLetterOrNumber).toBe(false);
     });
 
     test("Multiple number/letter inputs should return false", () => {
-      const input = new singleLettersAndNumbersToMorse("7778");
+      const input = new singleCharacterConverter("7778");
       expect(input.isLetterOrNumber).toBe(false);
     });
   });
 
   describe("getSingleMorseCode Return Test", () => {
     test("A single letter input (a-z or A-Z) should return its morse code equivalent", () => {
-      const input = new singleLettersAndNumbersToMorse("g");
+      const input = new singleCharacterConverter("g");
       expect(input.getSingleMorseCode).toBe("--.");
     });
 
     test("A single number input (0-9) should return its morse code equivalent", () => {
-      const input = new singleLettersAndNumbersToMorse(9);
+      const input = new singleCharacterConverter(9);
       expect(input.getSingleMorseCode).toBe("----.");
     });
 
     test("A valid punctuation mark (! , ? .) should return its morse code equivalent", () => {
-      const input = new singleLettersAndNumbersToMorse("?");
+      const input = new singleCharacterConverter("?");
       expect(input.getSingleMorseCode).toBe("..--..");
     });
 
-    test("Multiple number/letter inputs should return null", () => {
-      const input = new singleLettersAndNumbersToMorse({});
-      expect(input.getSingleMorseCode).toBe(null);
+    test("Multiple number/letter inputs should return undefined", () => {
+      const input = new singleCharacterConverter({});
+      expect(input.getSingleMorseCode).toBe(undefined);
+    });
+  });
+
+  describe("isMorseCode Validator Test", () => {
+    test("A single morse code input should return true", () => {
+      const input = new singleCharacterConverter(".---");
+      expect(input.isMorseCode).toBe(true);
+    });
+
+    test("Multiple morse code/incorrect morse code should return false", () => {
+      const input = new singleCharacterConverter(".._....");
+      expect(input.isMorseCode).toBe(false);
+    });
+
+    test("Anything that is not morse code should return false", () => {
+      const input = new singleCharacterConverter([]);
+      expect(input.isMorseCode).toBe(false);
+    });
+  });
+
+  describe("getSingleLetterOrNumber Return Test", () => {
+    test("A single morse code inpput should return its letter/number equivalent", () => {
+      const input = new singleCharacterConverter("-.-.--");
+      expect(input.getSingleLetterOrNumber).toBe("!");
+    });
+
+    test("Multiple/incorrect morse code inputs should return undefined", () => {
+      const input = new singleCharacterConverter(".--.-..");
+      expect(input.getSingleLetterOrNumber).toBe(undefined);
+    });
+
+    test("Inputs which aren't morse code, should return undefined", () => {
+      const input = new singleCharacterConverter(420);
+      expect(input.getSingleLetterOrNumber).toBe(undefined);
     });
   });
 });
