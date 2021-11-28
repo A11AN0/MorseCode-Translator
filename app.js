16b9d8e1164fe89067351b5bbd4eda1ffdc6bbc8
Allan <matrixboston7613@gmail.com>
2021-11-29 01:13:35 +0800

    changed the class LettersAndNumberToMorse to singleCharacterConverter. It can now convert both morse and alphabet/number characters

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-//Will simply convert a single letter/number to morse code
-export class singleLettersAndNumbersToMorse {
-  constructor(letterOrNumber) {
-    this.letterOrNumber = letterOrNumber.toString().toLowerCase(); //converts valid input to string and lowercase (if not already)
-  }
-
-  //Used regex & string length to verify that only 1 letter, number, comma, fullstop, exclamation or question-mark is inputted
-  get isLetterOrNumber() {
-    const input = this.letterOrNumber;
-    if (input.length != 1 || !input.match(/[a-z0-9!.,?]/g)) return false;
-    return true;
-  }
-
-  //Will now make a getter which when supplied a letter/number/symbol validated by isLetterOrNumber, will return a single morse code using switch cases/or objects
-  get getSingleMorseCode() {
-    const morseCodeDictionary = {
+/*Will simply convert a single letter/number to morse code, OR vice versa, iT cAn dO bOtH */
+export class singleCharacterConverter {
+  constructor(character) {
+    this.character = character.toString().toLowerCase();
+    this.morseCodeDictionary = {
       a: ".-",
       b: "-...",
       c: "-.-.",
@@ -55,8 +44,34 @@ export class singleLettersAndNumbersToMorse {
       "!": "-.-.--",
       ",": ".-.-.-",
     };
+  }
 
-    if (!this.isLetterOrNumber) return null;
-    return morseCodeDictionary[this.letterOrNumber];
+  /* Much easier to use an array of object keys to see if a letter input for conversion, 
+  is within the morseCodeDictionary */
+  get isLetterOrNumber() {
+    const input = this.character;
+    return Object.keys(this.morseCodeDictionary).includes(input);
+  }
+
+  /* Will now make a getter which when supplied a letter/number/symbol validated by 
+  isLetterOrNumber, will return a single morse code using switch cases/or objects */
+  get getSingleMorseCode() {
+    const input = this.character;
+    if (this.isLetterOrNumber) return this.morseCodeDictionary[input];
+  }
+
+  /*This getter will now check to see if the morse code input is valid, and can be converted*/
+  get isMorseCode() {
+    const input = this.character;
+    return Object.values(this.morseCodeDictionary).includes(input);
+  }
+
+  /*This will return the letter/key which matches the inputted morse code, if it is valid */
+  get getSingleLetterOrNumber() {
+    const input = this.character;
+    const keyArray = Object.keys(this.morseCodeDictionary);
+    const valueArray = Object.values(this.morseCodeDictionary);
+    const keyIndex = valueArray.indexOf(input);
+    if (this.isMorseCode) return keyArray[keyIndex];
   }
 }
