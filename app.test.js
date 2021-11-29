21cf1059cdd4bedb4c24d589ad5afe4f81cb1b4a
Allan <matrixboston7613@gmail.com>
2021-11-29 23:22:27 +0800

    All tests successful for the translator class

--- app.test.js
+++ app.test.js
@@ -1,6 +1,6 @@
 import { singleCharacterConverter, translator } from "./app.js";
 
-describe("Tests for the singleCharacterConverter class", () => {
+describe("------Tests for the singleCharacterConverter class------- \n", () => {
   describe("isLetterOrNumber Validator Test", () => {
     test("A single letter input (a-z or A-Z) should return true", () => {
       const input = new singleCharacterConverter("a");
@@ -73,14 +73,14 @@ describe("Tests for the singleCharacterConverter class", () => {
       expect(input.getSingleLetterOrNumber).toBe("");
     });
 
-    test("Inputs which aren't morse code, should return blank string", () => {
+    test("Inputs which aren't morse code, should return blank string \n", () => {
       const input = new singleCharacterConverter(420);
       expect(input.getSingleLetterOrNumber).toBe("");
     });
   });
 });
 
-describe("Tests for the translator class", () => {
+describe("------Tests for the translator class------- \n", () => {
   describe("getToMorse Return Test", () => {
     test("A sentence of valid letters/numbers and punctuation should return a morse equivalent", () => {
       const input = new translator(
@@ -90,21 +90,38 @@ describe("Tests for the translator class", () => {
         ".- / .--. .-. ----- ..-. --- ..- -. -.. / ... .. .-.. . -. -.-. . / .... .- ... / . -. - . .-. . -.. / - .... . / -.-. .... .- - .-.-.- .-.-.- .-.-.-"
       );
     });
-  });
 
-  describe("getToMorse Return Test", () => {
     test("A sentence of invalid values/punctuation should return a blank string", () => {
       const input = new translator("[][][]+[][][]#");
       expect(input.getToMorse).toBe("");
     });
-  });
 
-  describe("getToMorse Return Test", () => {
-    test("Should ignore the invalid characters and return the morse equivalent string for valid characters", () => {
+    test("Should ignore the invalid characters and return the morse equivalent string for valid characters when combined", () => {
       const input = new translator("[][][]pr0found[][][]# silence");
       expect(input.getToMorse).toBe(
         ".--. .-. ----- ..-. --- ..- -. -.. / ... .. .-.. . -. -.-. ."
       );
+    });
+  });
+
+  describe("getToAlphabet Return Test", () => {
+    test("A sentence of valid morse codes should return a lowercase string equivalent", () => {
+      const input = new translator(
+        ".-- .... .- - / .. ... / -.-- --- ..- .-. / ..-. .- ...- --- ..- .-. .---- - . / ..-. --- --- -.. ..--.."
+      );
+      expect(input.getToAlphabet).toBe("what is your favour1te food?");
+    });
+
+    test("A sentence of invalid morse code characters should return a blank string", () => {
+      const input = new translator("[];9 98{}  abcde ");
+      expect(input.getToAlphabet).toBe("");
+    });
+
+    test("Should ignore invalid characters and return the equivalent alphabet string for valid morse code when combined", () => {
+      const input = new translator(
+        "7485 ..-. .- ...- --- ..- .-. .---- - .  83[][]/ / ..-. --- --- -.. "
+      );
+      expect(input.getToAlphabet).toBe("favour1te food");
     });
   });
 });
